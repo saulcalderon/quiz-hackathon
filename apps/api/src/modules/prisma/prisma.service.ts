9c9ff928367c9fa0be5105ed7f68e49fb845efef
Saul Calderon <saulcalderon1408@gmail.com>
2026-01-31 11:19:42 -0600

    fix: prisma service

--- apps/api/src/modules/prisma/prisma.service.ts
+++ apps/api/src/modules/prisma/prisma.service.ts
@@ -5,21 +5,6 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(PrismaService.name);
 
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-      // log:
-      //   process.env.NODE_ENV === 'development'
-      //     ? ['query', 'info', 'warn', 'error']
-      //     : ['error'],
-      // errorFormat: 'minimal',
-    });
-  }
-
   async onModuleInit() {
     try {
       const startTime = Date.now();
